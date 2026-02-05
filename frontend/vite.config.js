76de78327047482269c99002edc9a1ec0bff1d18
NawaphonGamasa <6510210144@psu.ac.th>
2026-02-05 16:10:37 +0700

    เปลี่ยน port กับเพิ่ม scriptrun พร้อมกัน

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5174,  // เปลี่ยนเลข Port ตรงนี้ได้ตามใจชอบเลย
+    strictPort: true, // (Option) บังคับใช้เลขนี้เท่านั้น ถ้าชนให้ Error เลย ไม่ต้องรัน
+  },
 })
